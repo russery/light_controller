8943e8ad99e21d9d5a5805d4ebdbef8e0b191ee9
Robert Ussery <russery@gmail.com>
2019-07-22 22:11:53 -0700

    Functioning receive algorithm, needs cleanup and testing

--- software/gen_test_seq.py
+++ software/gen_test_seq.py
@@ -1,27 +1,53 @@
 
-import textwrap
+kPreambleLen = 20
+kDataLen = 16
+kParityBits = 1 # TODO change this out for a hamming code, or maybe a CRC https://www.w3.org/TR/PNG-CRCAppendix.html
+kBitDuration_us = 50
 
-testseq = 0xF0000aa123456789ABCDEFF123456789A
+testseq = 0x1234
+print("input:\n0x{0:X}\n".format(testseq))
 
-print("input: {0:X}\n".format(testseq))
+# Generate bit sequence
+bintext = "{:016b}".format(testseq)
+print("binary:\n{}\n".format(bintext))
 
-bintext = "{:0b}\n".format(testseq)
-print("binary: {}\n".format(bintext))
 
-outbin = ""
+# Calculate parity on just data segment
+parity = str(bintext.count('1') % 2)
+
+# Add preamble and start sequence
+preamble = ""
+for i in range(0,kPreambleLen):
+	preamble += "1"	#Preamble is all 1's, relies on manchester encoding to generate edges
+
+start_seq = "0"
+
+# Assemble preamble, data, start sequence, and parity
+bintext = preamble + start_seq + bintext + parity
+
+print("Complete message (preamble, start, data, parity):\n{}".format(bintext))
+
+# Generate Manchester Encoding
+bintext_manchester = ""
 for b in bintext:
 	if b == "0":
-		outbin += "00000"
-	else:
-		outbin += "11111"
+		bintext_manchester += "10"
+	if b == "1":
+		bintext_manchester += "01"
 
-outbin = int(outbin, 2)
-print("output binary: {0:b}\n".format(outbin))
+print("Encoded in Manchester Encoding:\n{}\n".format(bintext_manchester))
 
-outhex = "{0:X}".format(outbin)
-print("output hex: {}\n".format(outhex))
+# Generate nominal timing
+time = 0.0
+output = ""
+for x in bintext_manchester:
+	# Add random noise in front of data
+	# Add noise (random bit flips of duration << bit duration)
+	line = "{}, {}".format(time, x) 
+	output += line + "\r\n"
+	print(line)
+	time += kBitDuration_us / 1e6 # Write in seconds
 
-print("output array: {}".format(', 0x'.join(textwrap.wrap(outhex, 2))))
 
-
-
+with open('generated_packet.csv', 'w') as f:
+	f.write(output)
--- software/rf_devel.py
+++ software/rf_devel.py
@@ -5,8 +5,9 @@ import pandas
 import pdb
 
 
-SOURCE_FILE_NAME = 'transmitted_data.csv'
+#SOURCE_FILE_NAME = 'transmitted_data.csv'
 #SOURCE_FILE_NAME = 'received_data.csv'
+SOURCE_FILE_NAME = 'generated_packet.csv'
 
 
 df = pandas.read_csv(SOURCE_FILE_NAME, header=None, names=['t', 'val'])
@@ -35,16 +36,24 @@ class SampleBuffer:
 	def put(self, val):
 		self.vals[self.cnt] = val;
 		self.cnt += 1
-		if self.cnt > self.len:
+		if self.cnt == self.len:
 			pdb.set_trace()
 			raise Exception('Overflowed sample buffer')
+
+	def is_full(self):
+		return self.cnt == (self.len - 1)
+
+	def peek(self):
+		if(self.cnt > 0):
+			return self.vals[self.cnt-1]
+		else:
+			return -1
 
 	def flush(self):
 		self.vals = np.zeros(self.len)
 		self.cnt = 0
 
 
-kMinCorrelation = 4
 def CheckBitCorrelation(samples, expected_bit):
 	# TODO: find a better correlation algorithm. This rewards longer bits. Normalize to bit duration?
 	c = 0
@@ -54,48 +63,64 @@ def CheckBitCorrelation(samples, expected_bit):
 	return c
 
 
-def ProcessSample(sample):
-	#TODO: Decode Manchester encoding on data segment
-	pass
+def ProcessSample(samples):
+	sym_len = int(len(samples) / 2)
+	one_val = int(sym_len/2) + 1
+	assert(sym_len*2 == len(samples)) # shouldn't be receiving an odd number of samples
+	sym1 = sum(samples[0:sym_len-1]) >= one_val
+	sym2 = sum(samples[sym_len:-1]) >= one_val
+	if sym1 and not sym2: # 0b10
+		return 0
+	if not sym1 and sym2: # 0b01
+		return 1
+	pdb.set_trace()
+	raise Exception("invalid manchester bit sequence from samples {}".format(samples))
+	
 
 
-kLoopDuration_us = 1000.0
-kMinBitDuration_us = 4000.0
-kMaxBitDuration_us = 6000.0
+
+kLoopDuration_us = 10.0
+kMinBitDuration_us = 40.0
+kMaxBitDuration_us = 60.0
+kMinCorrelation = int(kMaxBitDuration_us/kLoopDuration_us*0.8)
 kMaxSampleCnt = int(kMaxBitDuration_us/kLoopDuration_us * 5) # Need to store up to five bit periods of data
-sb = SampleBuffer(kMaxSampleCnt)
+sb = SampleBuffer(kMaxSampleCnt+1)
 kStates = ['kWaitRisingEdge', 'kSync', 'kAlign', 'kFinishPreamble', 'kData']
+kPreambleLen = 20
+kDataLen = 16
 state_ = kStates[0]
 samples_remaining_ = 0
-def DoRfReceive(sample, time):	
-	global state_, sb, samples_remaining_
-	print("{}, {}, {}".format(state_, time, sample))
+bit_offset = 0
+bit_dur = 1
+bit_cnt = 0
+data = 0
+def DoRfReceive():	
+	global state_, sb, samples_remaining_, bit_offset, bit_dur, bit_cnt, data
+	#TODO Make sure interrupts are disabled when we're doing this for real to avoid conflicts on sample buffer
 	if state_ == 'kWaitRisingEdge':
-		if sample != 0:
+		if sb.peek() == 1:
 			state_ = 'kSync'
-			sb.put(sample)
-			print('Found edge at {}'.format(time))
+		sb.flush()
 	elif state_ == 'kSync':
-		sb.put(sample)
-		if sb.cnt == kMaxSampleCnt:
+		if sb.is_full():
 			# Find bit offset and duration that maximizes correlation with [1,0]
 			bit_correlation = 0
 			bit_offset = 0
 			bit_dur = 1
-			for o in range(0,(int(kMaxBitDuration_us/kLoopDuration_us)*2)+1): # Offsets from 0 up to a two bit periods
+			for o in range(0,(int(kMaxBitDuration_us/kLoopDuration_us)*2)+1): # Offsets from 0 up to two bit periods
 				for d in range(int(kMinBitDuration_us/kLoopDuration_us), int(kMaxBitDuration_us/kLoopDuration_us)+1): # Allow durations within valid range
-					correlation = CheckBitCorrelation(sb.vals[o:o+d], 1)
-					correlation += CheckBitCorrelation(sb.vals[o+d:o+(d*2)], 0)
-					correlation += CheckBitCorrelation(sb.vals[o+(d*2):o+(d*3)], 1)
+					correlation = 0
+					for b in range(0,6):
+						correlation += CheckBitCorrelation(sb.vals[o+(d*b):o+(d*(b+1))], (b+1)%2)
 					print('Correlation {} for offset {} and duration {}'.format(correlation, o, d))
 					if correlation >= bit_correlation: # Better than last best
 						bit_correlation = correlation
 						bit_offset = o
 						bit_dur = d
 
-			if bit_correlation > (kMinCorrelation * 2):
-				print('Found 0b10 sequence with best offset {} and duration {} and correlation {}'.format(bit_offset, bit_dur, bit_correlation))
-				samples_remaining_ = bit_dur - ((kMaxSampleCnt - bit_offset) % bit_dur)
+			if bit_correlation > (kMinCorrelation * 4):
+				print('Found preamble sequence with best offset {} and duration {} and correlation {}'.format(bit_offset, bit_dur, bit_correlation))
+				samples_remaining_ = bit_dur - ((kMaxSampleCnt - bit_offset) % bit_dur) - 1
 				sb.flush() # TODO, ideally we wouldn't discard the samples we've received so far
 				state_ = 'kAlign'
 			else:
@@ -107,30 +132,52 @@ def DoRfReceive(sample, time):
 		print('Aligning by receiving {} more bits'.format(samples_remaining_))
 		samples_remaining_ -= 1
 		if samples_remaining_ == 0:
+			sb.flush() # TODO ideally we'd check this bit against an expected sample
+			bit_cnt = 0
 			state_ = 'kFinishPreamble'
-		# TODO ideally we'd check this bit against an expected sample
 
 	elif state_ == 'kFinishPreamble':
-		pdb.set_trace()
-		ProcessSample(sample)
-		# Check for end of preamble sequence in bit buffer
-			# if we have it, flush the buffer
-		# Else if we've received more bits than expected in the preamble, reset to kWaitRisingEdge
-		# TODO Periodically re-sync on edges
+		if sb.cnt == bit_dur * 2:
+			if ProcessSample(sb.vals[0:bit_dur*2]) == 0: # Received stop bit.
+				print('Detected Start bit')
+				bit_cnt = 0
+				data = 0
+				state_ = 'kData'
+			else:
+				bit_cnt += 1
+				if bit_cnt > kPreambleLen:
+					print('Preamble too long, restarting')
+					state_ = 'kWaitRisingEdge'
+			sb.flush()
+		# TODO Periodically re-sync on edges? Depending on how well aligned we are this might not be necessary.
+
 	elif state_ == 'kData':
-		ProcessSample(sample)
-		pass
+		if sb.cnt == bit_dur * 2:
+			bit_cnt += 1
+			bit = ProcessSample(sb.vals[0:bit_dur*2])
+			data = data << 1
+			data += bit
+			print("{} Got data bit 0b{:b}".format(bit_cnt, bit))
+			if bit_cnt == (kDataLen + 1): # Received all data and parity bit
+				# Check parity:
+				p = 0
+				d = data >> 1
+				while d != 0:
+					if d % 2:
+						p += 1
+					d = d >> 1
+				p = p % 2
+				if p != (data % 2):
+					print('BAD PARITY {}'.format(p))
+				# Process packet:
+				print('Data received:\n0x{:X}'.format(data>>1))
+				# Reset state machine
+				state_ = 'kWaitRisingEdge'
+			sb.flush()
 
-# 000000000111100001111000011110000
-# 100000000101100001110000011110010
-# 10101010101110100
-# 100 1111 0000 1
 
-
-
-# Wait for an edge
-# Edge received, wait up to kMaxBitDuration_us for another edge
-
-for t in np.arange(min(df['t']), max(df['t']), kLoopDuration_us):
-	DoRfReceive(sample_rf_(t, df), t)
+for t in np.arange(min(df['t']), max(df['t']+kMaxBitDuration_us), kLoopDuration_us):
+	sb.put(sample_rf_(t, df))
+	#print(t)
+	DoRfReceive()
 
